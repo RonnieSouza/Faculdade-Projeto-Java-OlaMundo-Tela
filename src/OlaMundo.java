33175fa26891149eae6c6f423ec29128630d1436
Ronnie <ronnievicente.souza@hotmail.com>
2021-07-07 21:11:57 -0300

    adicionar arquivos ola mundo com tela atividade do curso basico de programação java

---
+++ src/OlaMundo.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class OlaMundo {
+    private JButton cliqueAquiButton;
+    private JLabel aquiVaiAparecerALabel;
+
+    public OlaMundo() {
+        cliqueAquiButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
